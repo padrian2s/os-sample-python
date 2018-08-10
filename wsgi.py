7d99beeb8e523712d9719cd5e078b61dbf5c4392
root <root@localhost.localdomain>
2018-08-10 16:20:35 -0400

    fix syntax error

--- wsgi.py
+++ wsgi.py
@@ -6,14 +6,15 @@ def hello():
     return "Running model srv v1.0\n entry points /init /run /cleanup\n"
 
 @application.route("/init")
-def init()
+def init():
     return "Initialize the model"
     
 @application.route("/run")
-def run()
+def run():
     return "Running model...\nDone."
     
 @application.route("/clean")
+def clean():
     return "Cleanup...\nComplete!"
 
 if __name__ == "__main__":
